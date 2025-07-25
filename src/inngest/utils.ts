3df139b3d46338ad158ca83260d625ce50bd676c
Nadjem-47 <nadgmo@gmail.com>
2025-07-25 18:42:45 +0100

    Add agent tools

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,6 +1,15 @@
 import { Sandbox } from "@e2b/code-interpreter"
+import { AgentResult, TextMessage } from "@inngest/agent-kit"
 
 export async function getSandbox(sandboxID: string) {
   const sandbox = await Sandbox.connect(sandboxID)
   return sandbox
+}
+
+
+
+export function lastAssistantTextMessageContent(result: AgentResult): string | undefined {
+  const index = result.output.findIndex((message) => message.role === "assistant")
+  const message = result.output[index] as | TextMessage | undefined
+  return message?.content ? typeof message.content === "string" ? message.content : message.content.map(c => c.text).join("") : undefined
 }
